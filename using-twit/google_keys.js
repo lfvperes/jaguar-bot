1b415056559f409413d7536f3444be08b124dd99
lfvperes <luisfvperes@gmail.com>
2021-02-02 11:42:51 -0300

    Removing deprecated files
    All keys were regenerated before the repo went public and are now stored only locally. The ones in previous commits are currently useless. Other files were removed from the remote but are still being used locally as reference while better methods are built.

--- using-twit/google_keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    //key: "AIzaSyCZuyzCyABg25HhNCHf451ZmrIlvdbz-OI",     //pse
-    key: 'AIzaSyCCTtnwqSWuXu5LVro_R9O08nafjrRm0cw',     // CSE
-    cx: '820675ee8f1f28e4f'                             // SE ID
-};
--- using-twit/jaguar_bot_test/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key:         "GCBWAJ2wVPuQVTDlr9EioZ9x8",
-    consumer_secret:      "y5IbcuH5x5svmQNw6CMsnlmJAyQWR3b3PvXFUMBWMrdTIb4Jml",
-    access_token:         "52150315-IP5noZeu1Lf4aq05TFhnkSNupd6Y3niBaRwRMNlix",
-    access_token_secret:  "cXEQ8e5myRXIH04AlLmXQAl6nwZ5y2M142qTycQ3Ml5F1"
-};
