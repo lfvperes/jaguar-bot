c8d75b22ad3ad7d8efdd22c97bb76dd23ef2f41e
lfvperes <luisfvperes@gmail.com>
2021-02-09 22:16:32 -0300

    Adding image downloader

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const Vision = require('./Vision');
 const twt_keys = require('./config/twitter-api');
 // const g_keys = require('./config/google-api');
 const bing_search_keys = require('./config/bing-search-api');
-const cognitive_keys = require('./config/cognitive-api');
+const bing_cogn_keys = require('./config/cognitive-api');
+
 const results = {
   full: './data/full_results.json',
   url: './data/url_results.json'
@@ -38,26 +39,7 @@ const bot = new Bot(client);
 const scraper = new Scraper(bing_search_keys, dog_results);
 const vision = new Vision(cognitive_keys, vis_res);
 
-scraper.bing_img_search('black dog',50);
-
 // bot.tweet_game();
 // bot.tweet_media(img);
-// scraper.bing_img_search('jaguar',5,22);
-/*
-if (fs.existsSync(results.url)) {
-  var url_list = JSON.parse(fs.readFileSync(results.url)).unfiltered;
-} else {
-  console.log('nao deu');
-  return;
-}
-for (const url of url_list) {
-  vision.analyze('Tags', url);
-}*/
-/*
-vision.analyze(
-  'Tags',
-  // 'https://cdn.pixabay.com/photo/2019/09/01/12/59/car-4445171_960_720.jpg',
-  'https://cdn.pixabay.com/photo/2018/06/10/12/52/black-panther-3466399_960_720.jpg',
-  true
-);
-*/
+
+// scraper.bing_img_search('black dog',50,50);
